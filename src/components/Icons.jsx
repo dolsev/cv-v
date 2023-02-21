5e253c18073a7129dabf9ad94dade0cfdf2cc453
dolsev <dolod777@yandex.ru>
2023-02-21 11:55:59 +0300

    text editing

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -7,7 +7,7 @@ const Icons = ({...props}) => {
             <a href="https://www.linkedin.com/in/vladislav-smirnov-914048154/"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn" /></a>
             <a href="https://hh.ru/resume/b476fa48ff0b21c62f0039ed1f763861665349"><img src="https://upload.wikimedia.org/wikipedia/commons/7/79/HeadHunter_logo.png" alt="HeadHunter.ru" /></a>
             <a href="https://www.instagram.com/andrey.da/"><img src="https://cdn-icons-png.flaticon.com/512/1409/1409946.png" alt="Instagram" /></a>
-            <a href="mailto:dolod777@yandex.ru"><img src="https://cdn-icons-png.flaticon.com/512/732/732200.png" alt="Email" /></a>
+            <a href="mailto:vladislav.smirnov.work@yandex.ru"><img src="https://cdn-icons-png.flaticon.com/512/732/732200.png" alt="Email" /></a>
 </div>
 
     );
