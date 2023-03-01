528256c6a0be9850ddb49baa9519f61a90bfab16
dolsev <dolod777@yandex.ru>
2023-03-02 00:12:48 +0300

    addition of some animations

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -4,7 +4,6 @@ const Avatar = ({imageUrl}) => {
     return (
         <div className='avatar-div' >
             <img src={imageUrl} alt="Avatar" className='avatar'/>
-            <img src='https://img.hhcdn.ru/photo/688977644.jpeg?t=1677788368&h=E2uhylBHlUvKLi8Dk9vTKw' alt="Avatar2" className='avatar2'/>
         </div>
 
     );
