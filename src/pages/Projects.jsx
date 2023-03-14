b2f312a2d03fe3b10e0bd913b4a6d93b2e952247
dolsev <dolod777@yandex.ru>
2023-03-14 13:14:30 +0300

    rethinking the cvv

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -14,9 +14,9 @@ const Projects = function (){
                             <h3>FURNITURE E-COMMERCIAL</h3>
                             <p>A furniture web-store website is an online platform that allows users to purchase a furniture.
                                 Filters, routing, Redux cart functionality and web-site design.</p>
-                            <p><strong>React, SCSS, Redux</strong></p>
+                            <p><strong>React-SCSS-Redux</strong></p>
                         </div>
-                        <img className='imgProject' alt='' src='/furniture-pic.jpg'/>
+                        <img className='imgProject' alt='Furniture e-commercial' src='/furniture-pic.jpg'/>
                         </Link>
                     </div>
                     <hr/>
@@ -26,13 +26,26 @@ const Projects = function (){
                                 <h3>GAMING-COMPANY</h3>
                                 <p>A landing selling page for game-developers company with presentation of investors and Roadmap for development process.
                                     </p>
-                                <p><strong>React, Styled Components</strong></p>
+                                <p><strong>React-Styled Components</strong></p>
                             </div>
-                            <img className='imgProject' alt='' src='/gamedev-pic.png'/>
+                            <img className='imgProject' alt='Gaming Company' src='/gamedev-pic.png'/>
                         </Link>
                     </div>
+                    <hr/>
+                    <div className='projects'>
+                        <div className='project'>
+                            <Link className='projLink' to={'https://phenomenal-heliotrope-4f6d1e.netlify.app/'}>
+                                <div className='projDescription'>
+                                    <h3>CHAT EZ</h3>
+                                    <p>A chat micro-service built for web-site based on API.</p>
+                                    <p><strong>React-SCSS</strong></p>
+                                </div>
+                                <img className='imgProject' alt='' src='/chat-pic.jpg'/>
+                            </Link>
+                        </div>
                 </div>
     </div>
+        </div>
         </div>
 }
 export default Projects
