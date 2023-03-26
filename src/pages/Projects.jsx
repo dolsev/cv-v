98d1b52b2135d05bcec666e63bef04665e702a19
dolsev <dolod777@yandex.ru>
2023-03-26 11:31:35 +0300

    added book project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -21,15 +21,29 @@ const Projects = function (){
                         <a className='icon' href="https://github.com/dolsev/cozy_furniture"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" />Code</a>
                     </div>
                     <hr/>
+                    <div className='projects'>
+                        <div className='project'>
+                            <Link className='projLink' to={'https://dolsev.github.io/bookapp-search/'}>
+                                <div className='projDescription'>
+                                    <h3>BOOK-SEARCH</h3>
+                                    <p>Website, allowing users to look for their favourite books included into Google Book API. Comes with sorting and filtering functionality along with means to check detailed description of the books.</p>
+                                    <p><strong>React-Redux-API</strong></p>
+                                </div>
+                                <img className='imgProject' alt='BookSearch' src='/BookSearch.png'/>
+                            </Link>
+                            <a className='icon' href="https://dolsev.github.io/bookapp-search/"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" />Code</a>
+                        </div>
+                    </div>
+                    <hr/>
                     <div className='project'>
                         <Link className='projLink' to={'https://amazing-parfait-82489c.netlify.app/'}>
                             <div className='projDescription'>
-                                <h3>GAMING-COMPANY</h3>
+                                <h3>FW GAMING</h3>
                                 <p>A landing selling page for game-developers company with presentation of investors and Roadmap for development process.
                                     </p>
                                 <p><strong>React-Styled Components</strong></p>
                             </div>
-                            <img className='imgProject' alt='Gaming Company' src='/gamedev-pic.png'/>
+                            <img className='imgProject' alt='FW GAMING' src='/gamedev-pic.png'/>
                         </Link>
                         <a className='icon' href="https://github.com/dolsev/game-website"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" />Code</a>
 
@@ -37,13 +51,13 @@ const Projects = function (){
                     <hr/>
                     <div className='projects'>
                         <div className='project'>
-                            <Link className='projLink' to={'https://phenomenal-heliotrope-4f6d1e.netlify.app/'}>
+                            <Link className='projLink' to={'https://dolsev.github.io/potteriana-characters/'}>
                                 <div className='projDescription'>
                                     <h3>POTTER CHARACTERS</h3>
                                     <p>A simple app, which allows you to choose your favourite characters from Harry Potter and filter them by your likes. You can even delete them entirely :).</p>
                                     <p><strong>React-Redux-LocalStorage</strong></p>
                                 </div>
-                                <img className='imgProject' alt='' src='/hp-pic.png'/>
+                                <img className='imgProject' alt='POTTER CHARACTERS' src='/hp-pic.png'/>
                             </Link>
                             <a className='icon' href="https://github.com/dolsev/potteriana-characters"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" />Code</a>
                         </div>
@@ -55,9 +69,9 @@ const Projects = function (){
                                 <div className='projDescription'>
                                     <h3>CHAT EZ</h3>
                                     <p>A chat micro-service built for web-site based on API.</p>
-                                    <p><strong>React-SCSS</strong></p>
+                                    <p><strong>React-SCSS-API</strong></p>
                                 </div>
-                                <img className='imgProject' alt='' src='/chat-pic.jpg'/>
+                                <img className='imgProject' alt='CHAT EZ' src='/chat-pic.jpg'/>
                             </Link>
                             <a className='icon' href="https://github.com/dolsev/chat-app-APIs"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" />Code</a>
                         </div>
